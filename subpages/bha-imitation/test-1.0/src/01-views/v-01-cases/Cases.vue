5b2969adb379e196dc28300ed4b0d1c36945bae2
Julian Friedemann <julian.friedemann@db-n.com>
2019-09-16 14:53:22 +0200

    First portfolio item added!

---
+++ subpages/bha-imitation/test-1.0/src/01-views/v-01-cases/Cases.vue
@@ -0,0 +1,62 @@
+<template>
+  <transition name="menu_transition">
+    <div id="cases" @mousewheel="side_scroll">
+      <logo/>
+      <navMenu/>
+
+      <caseItem v-for="(cas_e, key) in cases" v-bind="cas_e" :key="key"/>
+
+      <scrollButtons/>
+    </div>
+  </transition>
+</template>
+<script>
+import cases from "@/05-data/Cases.json";
+export default {
+  data() {
+    return {
+      cases,
+      x: ""
+    };
+  },
+  methods: {
+    side_scroll(event) {
+      let y = event.deltaY;
+      this.x = scrollX;
+      if (y >= 0) {
+        window.scrollBy({
+          left: 75
+        });
+      } else {
+        window.scrollBy({
+          left: -75
+        });
+      }
+    }
+  },
+  watch: {
+    x: function() {
+      const sbtnL = document.getElementById("SbtnL").classList;
+      const sbtnR = document.getElementById("SbtnR").classList;
+
+      let end =
+        Object.keys(this.cases).length *
+          document.getElementById("items").offsetWidth -
+        window.innerWidth +
+        4;
+
+      if (scrollX <= 100) {
+        sbtnL.add("hide");
+      } else if (scrollX >= end - 100 && scrollX <= end) {
+        sbtnR.add("hide");
+      } else {
+        sbtnL.remove("hide");
+        sbtnR.remove("hide");
+      }
+    }
+  }
+};
+</script>
+<style lang="scss" src="./Cases.scss">
+</style>
+
---
+++ subpages/bha-imitation/test-1.0/src/03-components/c-01-case-item/case_item.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="images" @mouseover="show = true" @mouseleave="show = false">
+    <div id="items" :style="{background: BI}">
+      <div class="casetext">
+        <h2 class="case">{{ headline }}</h2>
+        <transition name="fade">
+          <p class="hovertext" v-if="show">{{ text }}</p>
+        </transition>
+        <h3 class="brand">{{ brand }}</h3>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      BI: this.backgroundImage
+    };
+  },
+  props: {
+    headline: String,
+    text: String,
+    brand: String,
+    backgroundImage: String
+  }
+};
+</script>
+<style lang="scss" src="./case_item.scss">
+</style>
