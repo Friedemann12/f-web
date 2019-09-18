6e0dcf21a627c7a3c0553f063148a9d56566779e
Julian Friedemann <julian.friedemann@db-n.com>
2019-09-18 11:40:03 +0200

    only real item in portfolio

--- index.php
+++ index.php
@@ -98,7 +98,7 @@
             </div>
 
             <!-- Portfolio Item 2 -->
-            <div class="col-md-6 col-lg-4">
+<!--            <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
@@ -107,10 +107,10 @@
                     </div>
                     <img class="img-fluid" src="img/portfolio/cake.png" alt="">
                 </div>
-            </div>
+            </div>-->
 
             <!-- Portfolio Item 3 -->
-            <div class="col-md-6 col-lg-4">
+<!--            <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal3">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
@@ -119,10 +119,10 @@
                     </div>
                     <img class="img-fluid" src="img/portfolio/circus.png" alt="">
                 </div>
-            </div>
+            </div>-->
 
             <!-- Portfolio Item 4 -->
-            <div class="col-md-6 col-lg-4">
+<!--            <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal4">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
@@ -131,10 +131,10 @@
                     </div>
                     <img class="img-fluid" src="img/portfolio/game.png" alt="">
                 </div>
-            </div>
+            </div>-->
 
             <!-- Portfolio Item 5 -->
-            <div class="col-md-6 col-lg-4">
+<!--            <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal5">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
@@ -143,10 +143,10 @@
                     </div>
                     <img class="img-fluid" src="img/portfolio/safe.png" alt="">
                 </div>
-            </div>
+            </div>-->
 
             <!-- Portfolio Item 6 -->
-            <div class="col-md-6 col-lg-4">
+<!--            <div class="col-md-6 col-lg-4">
                 <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal6">
                     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
                         <div class="portfolio-item-caption-content text-center text-white">
@@ -155,7 +155,7 @@
                     </div>
                     <img class="img-fluid" src="img/portfolio/submarine.png" alt="">
                 </div>
-            </div>
+            </div>-->
 
         </div>
         <!-- /.row -->
