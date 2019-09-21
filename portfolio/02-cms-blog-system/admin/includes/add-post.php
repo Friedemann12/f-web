a185477bec1b9ba00b3f8435339c49a35106eec9
acr103 <julian.friedemann@haw-hamburg>
2019-09-21 21:19:00 +0200

    fixing add post issue

--- portfolio/02-cms-blog-system/admin/includes/add-post.php
+++ portfolio/02-cms-blog-system/admin/includes/add-post.php
@@ -9,13 +9,13 @@ if (isset($_POST["create_post"])) {
     $post_img = escape($_FILES["post_img"]["name"]);
     $post_img_temp = escape($_FILES["post_img"]["tmp_name"]);
     $post_date = date("y-m-d");
-    $post_com_count = 4;
+    $post_com_count = 0;
     move_uploaded_file($post_img_temp, "../images/$post_img");
     $img_dir = "../images/$post_img";
     $query = "INSERT INTO posts(post_cat_id, post_title, post_author, post_date, post_img, post_content, post_tags, post_com_count) ";
     $query .= "VALUES('{$post_cat_id}','{$post_title}','{$post_author}','{$post_date}','{$img_dir}','{$post_content}','{$post_tags}','{$post_com_count}')";
     $create_post_query = mysqli_query($con, $query);
-    global $create_post_query;
+    // global $create_post_query;
     if (!$create_post_query) {
         die("NO post created " . mysqli_error($con));
     } else {
