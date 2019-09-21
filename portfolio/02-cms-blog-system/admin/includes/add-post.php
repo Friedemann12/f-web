bc4144a9b4dd6a9653d84d203fb85a9352faf784
acr103 <julian.friedemann@haw-hamburg>
2019-09-21 21:08:29 +0200

    implementing logic for test user

--- portfolio/02-cms-blog-system/admin/includes/add-post.php
+++ portfolio/02-cms-blog-system/admin/includes/add-post.php
@@ -1,32 +1,20 @@
 <?php
-
 include "functions.php";
-
 if (isset($_POST["create_post"])) {
     $post_title = escape($_POST["post_title"]);
     $post_author = escape($_POST["post_author"]);
     $post_cat_id = escape($_POST["post_cat_id"]);
     $post_tags = escape($_POST["post_tags"]);
     $post_content = escape($_POST["post_content"]);
-
-
     $post_img = escape($_FILES["post_img"]["name"]);
     $post_img_temp = escape($_FILES["post_img"]["tmp_name"]);
-
     $post_date = date("y-m-d");
-    $post_com_count = $_POST["post_com_count"];
-
-
+    $post_com_count = 4;
     move_uploaded_file($post_img_temp, "../images/$post_img");
-
-    $img_dir = "images/$post_img";
-
+    $img_dir = "../images/$post_img";
     $query = "INSERT INTO posts(post_cat_id, post_title, post_author, post_date, post_img, post_content, post_tags, post_com_count) ";
     $query .= "VALUES('{$post_cat_id}','{$post_title}','{$post_author}','{$post_date}','{$img_dir}','{$post_content}','{$post_tags}','{$post_com_count}')";
-
-
     $create_post_query = mysqli_query($con, $query);
-
     global $create_post_query;
     if (!$create_post_query) {
         die("NO post created " . mysqli_error($con));
@@ -34,8 +22,6 @@ if (isset($_POST["create_post"])) {
         header("Location: posts.php?source=success-posting");
     }
 }
-
-
 ?>
 
 
@@ -47,18 +33,16 @@ if (isset($_POST["create_post"])) {
     </div>
     <div class="form-group">
         <label for="post_cat_id">Post Category</label>
-        <select name="post_cat_id" id="">
+        <select name="post_cat_id" id="" value="{post_id_for_edit}">
             <?php
             $query = "SELECT * FROM categories";
             $select_cat_id = mysqli_query($con, $query);
-
             if (!$select_cat_id) {
                 die("Query Failed " . mysqli_error($con));
             } else {
                 while ($row = mysqli_fetch_assoc($select_cat_id)) {
                     $cat_id = $row["cat_id"];
                     $cat_title = $row["cat_title"];
-
                     echo "<option value='{$cat_id}'>{$cat_title}</option>";
                 }
             }
