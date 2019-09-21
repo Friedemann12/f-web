1a7574ca99e7d770141a4969f20af61bfe6fee5c
acr103 <julian.friedemann@haw-hamburg>
2019-09-21 20:10:39 +0200

    implementing logic for test user

--- portfolio/02-cms-blog-system/admin/includes/admin-header.php
+++ portfolio/02-cms-blog-system/admin/includes/admin-header.php
@@ -10,10 +10,10 @@ if (isset($_SESSION["user_role"])) {
     if ($_SESSION["user_role"] !== "admin") {
 
         header("Location: ../index.php");
-    } else if ($_SESSION["user_role"] !== "test_admin") {
+    } // else if ($_SESSION["user_role"] !== "test_admin") {
 
-        header("Location: ../index.php");
-    }
+    //     header("Location: ../index.php");
+    // }
 }
 
 ?>
