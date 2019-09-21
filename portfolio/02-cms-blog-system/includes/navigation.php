e592beda0edecf989fc0454f19a4c14961ce4bab
acr103 <julian.friedemann@haw-hamburg>
2019-09-21 12:14:13 +0200

    logout main page fixed

--- portfolio/02-cms-blog-system/includes/navigation.php
+++ portfolio/02-cms-blog-system/includes/navigation.php
@@ -43,7 +43,7 @@
 
                     echo '
                             <li>
-                                <a href = "../includes/logout.php">
+                                <a href = "./logout.php">
                                 <span class="glyphicon glyphicon-log-out"></span>
                                 <span>Logout</span>
                                 </a>
